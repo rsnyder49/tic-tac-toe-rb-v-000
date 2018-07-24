ff2c349a29ce99ea300e96833d3e361efba3d6ae
Robert Snyder <r.snyder49@gmail.com>
2018-07-24 22:50:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -117,13 +117,14 @@ end
 def play(board)
   until over?(board)
   turn(board)
+end
   if winner(board) === "X"
     puts "Congratulations X!"
   elsif winner(board) === "O"
     puts "Congratulations O!"
   elsif draw?(board)
     puts "Cat's Game!"
-  end
+  
   end
 end
 
